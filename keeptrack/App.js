264169e968dd8a852551780b1f3d7935cce2e65c
jamesthecooke <jamesjcooke03@gmail.com>
2024-02-17 22:46:43 +0000

    got the react native project set up. installed the react navigator and set up a screen to make sure its working

---
+++ keeptrack/App.js
@@ -0,0 +1,15 @@
+import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
