dab6b0bc7fac10cef46237813070150fa962f99b
jamesthecooke <jamesjcooke03@gmail.com>
2024-02-20 16:06:35 +0000

    styled the inner money type to better resemble the wireframes

--- keeptrack/components/InnerMoneyType.js
+++ keeptrack/components/InnerMoneyType.js
@@ -4,12 +4,17 @@ import { StyleSheet, Text, View, Image } from "react-native";
 function InnerMoneyType() {
   return (
     <View style={styles.container}>
-      <View style={styles.catContainer}>
+      <View style={styles.catImg}>
         <Image
           style={styles.img}
           source={require("../assets/icon.png")}
         ></Image>
+      </View>
+      <View style={styles.catText}>
         <Text style={styles.text}>blaaaaaaaaaaaa</Text>
+      </View>
+      <View style={styles.catValue}>
+        <Text style={styles.value}>£ 10.51</Text>
       </View>
     </View>
   );
@@ -18,9 +23,22 @@ function InnerMoneyType() {
 export default InnerMoneyType;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: "row",
+    width: "100%",
+    paddingLeft: 10,
+    margin: 4,
+  },
 
-  catContainer: {
+  catImg: {
+    backgroundColor: "#6B6C70",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+
+  catText: {
     flexDirection: "row",
     padding: 3,
     margin: 3,
@@ -28,18 +46,30 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     backgroundColor: "#6B6C70",
     borderRadius: 10,
-    width: "100%",
+    width: "45%",
+  },
+
+  catValue: {
+    flexDirection: "row",
+    backgroundColor: "#6B6C70",
+    borderRadius: 10,
+    alignItems: "center",
   },
   img: {
     width: 30,
     height: 30,
     borderRadius: 50,
-    marginRight: 10,
   },
 
   text: {
     color: "white",
     fontSize: 15,
     padding: 1,
+    marginRight: 3,
+  },
+  value: {
+    color: "white",
+    fontSize: 13,
+    marginRight: 3,
   },
 });
