1a9f2ed88ece3e26c8ae13467494eeb4c2aad5d2
jamesthecooke <jamesjcooke03@gmail.com>
2024-02-27 16:10:11 +0000

    changed app.tsx to app.js

--- keeptrack/App.js
+++ keeptrack/App.js
@@ -61,7 +61,7 @@ import Auth from "./components/auth";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
